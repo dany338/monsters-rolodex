05db02a12f00ff99cc67b13b286248b3d3568829
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-09-19 12:04:07 -0500

    Updates

---
+++ precache-manifest.26d0b3149a448f19196e6e2b93429509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4cac1d5c450f7a598d02dd093a4bf8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "66cc3afc7fa9773aa0e2",
+    "url": "/monsters-rolodex/static/css/main.5e3c87bb.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cc3afc7fa9773aa0e2",
+    "url": "/monsters-rolodex/static/js/main.521b9942.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
